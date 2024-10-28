6cb5bde8db095b5afa8ed2e05153b603baedcacf
dhendu1996@gmail.com <dhendu1996@gmail.com>
2024-10-28 14:04:33 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # leaflet-challenge
+### Part 1: Create the Earthquake Visualization
+
+Your first task is to visualize an earthquake dataset. Complete the following steps:
+
+Get your dataset. To do so, follow these steps:
+
+The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the USGS GeoJSON FeedLinks to an external site. page and choose a dataset to visualize. The following image is an example screenshot of what appears when you visit this link:
+
+![alt text](Images/2-BasicMap.png)
+
+When you click a dataset (such as "All Earthquakes from the Past 7 Days"), you will be given a JSON representation of that data. Use the URL of this JSON to pull in the data for the visualization. The following image is a sampling of earthquake data in JSON format:
+
+![alt text](Images/4-JSON.png)
+
+Import and visualize the data by doing the following:
+
+Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+
+Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+
+Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+
+Include popups that provide additional information about the earthquake when its associated marker is clicked.
+
+Create a legend that will provide context for your map data.
+
+Your visualization should look something like the preceding map.
+
+My map looks like the following:
+
+![alt text](<Images/Image 10-28-24 at 1.53 PM.png>)
+
+View of the final map can be visualized here: file:///Users/davidhenderson/Desktop/leaflet-challenge/index.html
+
+Resources: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
--- static/js/logic.js
+++ static/js/logic.js
@@ -9,16 +9,16 @@ let myMap = L.map("map", {
     center: [39.8283, -98.5795],  // Centered on the US
     zoom: 2,
     layers: [streetMap]  // Default layer is streetMap
-  }); 
+}); 
 
 // Define baseMaps to allow switching between Street and Topography layers
 let baseMaps = {
     "Street Map": streetMap,
-  };
+};
   
 let earthquake = new L.LayerGroup();
 
-let overlay = {"Earthquakes" : earthquake};
+let overlay = { "Earthquakes": earthquake };
 
 L.control.layers(baseMaps, overlay).addTo(myMap);
 
@@ -27,23 +27,22 @@ function styleInfo(feature) {
         color: chooseColor(feature.geometry.coordinates[2]),
         radius: chooseRadius(feature.properties.mag),
         fillColor: chooseColor(feature.geometry.coordinates[2])
-    }
-};
+    };
+}
 
 function chooseColor(depth) {
     if (depth <= 10) return "yellow";
-    else if (depth > 10 & depth <=20) return "green";
-    else if (depth > 20 & depth <=30) return "orange";
-    else if (depth > 30 & depth <=40) return "red";
-    else if (depth > 40 & depth <=50) return "blue";
-    else if (depth > 50 & depth <=60) return "purple";
-    else return "black"
-
-};
+    else if (depth > 10 && depth <= 20) return "green";
+    else if (depth > 20 && depth <= 30) return "orange";
+    else if (depth > 30 && depth <= 40) return "red";
+    else if (depth > 40 && depth <= 50) return "blue";
+    else if (depth > 50 && depth <= 60) return "purple";
+    else return "black";
+}
 
 function chooseRadius(magnitude) {
-    return magnitude*5;
-};
+    return magnitude * 5;
+}
 
 d3.json(url).then(function (data) {
     L.geoJson(data, {
@@ -53,19 +52,21 @@ d3.json(url).then(function (data) {
         style: styleInfo
     }).addTo(earthquake);
     earthquake.addTo(myMap);
+}).catch(function (error) {
+    console.error('Error fetching data:', error);
 });
 
-let legend = L.control({position: "bottomright"});
-legend.onAdd = function(myMap) {
-    let div = L.DomUtil.create("div","info legend")
-        div.innerHTML += "<h4>Depth Color Legend</h4>";
-        div.innerHTML += '<i style="background: yellow"></i><span>(Depth < 10)</span><br>';
-        div.innerHTML += '<i style="background: green"></i><span>(10 < Depth <= 20)</span><br>';
-        div.innerHTML += '<i style="background: orange"></i><span>(20 < Depth <= 30)</span><br>';
-        div.innerHTML += '<i style="background: red"></i><span>(30 < Depth <= 40)</span><br>';
-        div.innerHTML += '<i style="background: blue"></i><span>(40 < Depth <= 50)</span><br>';
-        div.innerHTML += '<i style="background: purple"></i><span>(50 < Depth <= 60)</span><br>';
-        div.innerHTML += '<i style="background: black"></i><span>(Depth > 60)</span><br>';
+let legend = L.control({ position: "bottomright" });
+legend.onAdd = function (myMap) {
+    let div = L.DomUtil.create("div", "info legend");
+    div.innerHTML += "<h4>Depth Color Legend</h4>";
+    div.innerHTML += '<i style="background: yellow"></i><span>(Yellow = Depth < 10)</span><br>';
+    div.innerHTML += '<i style="background: green"></i><span>(Green = 10 < Depth <= 20)</span><br>';
+    div.innerHTML += '<i style="background: orange"></i><span>(Orange = 20 < Depth <= 30)</span><br>';
+    div.innerHTML += '<i style="background: red"></i><span>(Red = 30 < Depth <= 40)</span><br>';
+    div.innerHTML += '<i style="background: blue"></i><span>(Blue = 40 < Depth <= 50)</span><br>';
+    div.innerHTML += '<i style="background: purple"></i><span>(Purple = 50 < Depth <= 60)</span><br>';
+    div.innerHTML += '<i style="background: black"></i><span>(Black = Depth > 60)</span><br>';
     return div;
 };
 legend.addTo(myMap);
